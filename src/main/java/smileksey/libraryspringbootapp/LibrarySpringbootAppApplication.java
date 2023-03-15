658a4d52dd97d8c2d44fae17d9fe6653cd465339
smileksey <smileksey@gmail.com>
2023-03-15 15:32:10 +0300

    Initial commit

---
+++ src/main/java/smileksey/libraryspringbootapp/LibrarySpringbootAppApplication.java
@@ -0,0 +1,13 @@
+package smileksey.libraryspringbootapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibrarySpringbootAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibrarySpringbootAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/smileksey/libraryspringbootapp/LibrarySpringbootAppApplicationTests.java
@@ -0,0 +1,13 @@
+package smileksey.libraryspringbootapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibrarySpringbootAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
